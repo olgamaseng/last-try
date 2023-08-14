76f140e6eb987743664d04c28a29a88732f4aaac
Olga Maseng <masengolga@gmail.com>
2023-08-14 10:19:53 +0200

    Update App.jsx

--- Mindful Moments/src/App.jsx
+++ Mindful Moments/src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import Header from './Components/Header';
 import SimpleBottomNavigation from './Components/MainNav';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
 
 
 
   return (
+
     <BrowserRouter>
       <Header />
       <div className='app'>
-        Home
+        <container>
+          <Switch>
+            <Route path='/' component={Trending} exact />
+            <Route path='/seasons' component={Seasons} />
+            <Route path='/episodes' component={Episodes} />
+            <Route path='/search' component={Search} />
+          </Switch>
+        </container>
       </div>
       <SimpleBottomNavigation />
     </BrowserRouter>
